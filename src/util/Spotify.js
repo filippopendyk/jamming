5e1248e48c234ba34f368a327ee342862bde4294
filippopendyk <filipos28511@gmail.com>
2022-09-28 22:45:46 -0400

    sets up spotify module

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,6 @@
 let accessToken;
+const clientId = 'ff90f41cfa5c4c0ebc1e2b3c831e44c2';
+const redirectUri = 'http://localhost:3000';
 
 const Spotify = {
     getAccessToken() {
@@ -12,6 +14,14 @@ const Spotify = {
 
         if (accessTokenMatch && expiresInMatch) {
             accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // This clears the parameters, allowing us to grab a new access token when it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
         }
     }
 }
